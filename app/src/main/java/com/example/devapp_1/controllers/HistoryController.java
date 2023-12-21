58bd424fd263174e450f4bf7e1d59736f27e7ddc
Ala Arfaoui <128066189+Ala075@users.noreply.github.com>
2023-12-21 15:00:03 +0100

    update HistoryController.java

--- app/src/main/java/com/example/devapp_1/controllers/HistoryController.java
+++ app/src/main/java/com/example/devapp_1/controllers/HistoryController.java
@@ -1,5 +1,6 @@
 package com.example.devapp_1.controllers;
 
+import com.example.devapp_1.models.History;
 import android.content.ContentValues;
 import android.content.Context;
 import android.database.Cursor;
@@ -20,6 +21,7 @@ public class HistoryController {
     private static final int NUM_COL_CONULT = 2;
     private SQLiteDatabase bd;
     private SQLiteHelper histories;
+    private History history;
 
     // Private constructor to enforce Singleton pattern
     private HistoryController(Context context) {
@@ -32,7 +34,7 @@ public class HistoryController {
             instance = new HistoryController(context.getApplicationContext());
         }
         return instance;
-    }
+    } 
 
     public void openForWrite() {
         bd = histories.getWritableDatabase();
@@ -50,29 +52,31 @@ public class HistoryController {
         return bd;
     }
 
-    public long addHistory(History history) {
+    public long addHistory(String username,String consult) {
+        history = new History(username,consult);
+      
         ContentValues content = new ContentValues();
         content.put(COL_NAME, history.getName());
         content.put(COL_CONSULT, history.getConsultation());
         return bd.insert(TABLE_NAME, null, content);
     }
 
-   // public int updateHistory(int id, History history) {
-//       ContentValues content = new ContentValues();
-//       content.put(COL_NAME, history.getName());
-//       content.put(COL_CONSULT, history.getConsultation());
-//       return bd.update(TABLE_NAME, content, COL_ID + " = " + id, null);
-//      }
-
-    public int removeHistory(String username) {
-        return bd.delete(TABLE_NAME, COL_NAME + " = ?", new String[]{username});
-    }
-
-    public History getHistory(String username) {
-        Cursor c = bd.query(TABLE_NAME, new String[]{COL_ID, COL_NAME, COL_CONSULT},
-                COL_NAME + " LIKE ?", new String[]{"%" + username + "%"}, null, null, COL_NAME);
-        return cursorToHistory(c);
-    }
+    // public int updateHistory(int id, History history) {
+//        ContentValues content = new ContentValues();
+//        content.put(COL_NAME, history.getName());
+//        content.put(COL_CONSULT, history.getConsultation());
+//        return bd.update(TABLE_NAME, content, COL_ID + " = " + id, null);
+//     }
+// 
+//     public int removeHistory(String username) {
+//         return bd.delete(TABLE_NAME, COL_NAME + " = ?", new String[]{username});
+//     }
+// 
+//     public History getHistory(String username) {
+//         Cursor c = bd.query(TABLE_NAME, new String[]{COL_ID, COL_NAME, COL_CONSULT},
+//                 COL_NAME + " LIKE ?", new String[]{"%" + username + "%"}, null, null, COL_NAME);
+//         return cursorToHistory(c);
+//     }
 
     public ArrayList<History> getAllHistories() {
         Cursor c = bd.query(TABLE_NAME, new String[]{COL_ID, COL_NAME, COL_CONSULT},
@@ -80,20 +84,20 @@ public class HistoryController {
         return cursorToHistoryList(c);
     }
 
-    private History cursorToHistory(Cursor c) {
-        if (c.getCount() == 0) {
-            c.close();
-            return null;
-        }
-        c.moveToFirst();
-        History history = new History();
-        history.setId(c.getInt(NUM_COL_ID));
-        history.setUserame(c.getString(NUM_COL_NAME));
-        history.setConsultation(c.getString(NUM_COL_CONSULT));
-        
-        c.close();
-        return history;
-    }
+    // private History cursorToHistory(Cursor c) {
+//         if (c.getCount() == 0) {
+//             c.close();
+//             return null;
+//         }
+//         c.moveToFirst();
+//         History history = new History();
+//         history.setId(c.getInt(NUM_COL_ID));
+//         history.setUserame(c.getString(NUM_COL_NAME));
+//         history.setConsultation(c.getString(NUM_COL_CONSULT));
+//         
+//         c.close();
+//         return history;
+//     }
 
     private ArrayList<History> cursorToHistoryList(Cursor c) {
         if (c.getCount() == 0) {
