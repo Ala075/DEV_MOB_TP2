74fbfb1d3dca1bdf138a1a31fabbfbfd90a7c685
Ala Arfaoui <128066189+Ala075@users.noreply.github.com>
2023-12-21 14:03:08 +0100

    update app/src/main/java/com/example/devapp_1/models/History.java

--- app/src/main/java/com/example/devapp_1/models/History.java
+++ app/src/main/java/com/example/devapp_1/models/History.java
@@ -1,4 +1,32 @@
 package com.example.devapp_1.models;
 
 public class History {
+  private int id;
+  private String username;
+  private String consultation;
+
+  public Chapitre() {}
+
+  public Chapitre (String name, String consult){
+    this.username = name;
+    this.consultation = consult; 
+  }
+
+  public int getId () { return id; }
+  public void setId (int id) { this.id = id; }
+
+  public String getUserame() { return username; }
+  public void setUserame (String name) { this.username = username; }
+  
+  public String getConsultation () { 
+    return consultation; 
+  }
+  public void setConsultation (String consult) { this.consultation = consult; }
+
+  @Override
+  public String toString() {
+    StringBuilder sb = new StringBuilder();
+    sb.append("Nom du patient = " + username + "\n" + "Consultation  = " + consultation);
+    return sb.toString(); 
+  }
 }
