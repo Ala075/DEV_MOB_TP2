30d5f2f56a9006b8128c2b23f530137a18594432
Ala Arfaoui <128066189+Ala075@users.noreply.github.com>
2023-12-21 16:00:30 +0100

    update History.java

--- app/src/main/java/com/example/devapp_1/models/History.java
+++ app/src/main/java/com/example/devapp_1/models/History.java
@@ -16,7 +16,7 @@ public class History {
   public void setId (int id) { this.id = id; }
 
   public String getUsername() { return username; }
-  public void setUsername (String name) { this.username = username; }
+  public void setUsername (String username) { this.username = username; }
   
   public String getConsultation () { 
     return consultation; 
