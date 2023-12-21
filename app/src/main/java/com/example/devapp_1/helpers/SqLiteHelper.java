24cb28b6cad487c4710b286ef374a387e38e1369
Ala Arfaoui <128066189+Ala075@users.noreply.github.com>
2023-12-21 22:05:16 +0100

    update SqLiteHelper.java

---
+++ app/src/main/java/com/example/devapp_1/helpers/SqLiteHelper.java
@@ -0,0 +1,29 @@
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+public class SqLiteHelper extends SQLiteOpenHelper {
+  private static final String TABLE_NAME = "table_histories";
+  public static final String COL_ID = "ID";
+  public static final String COL_NAME = "USERNAME";
+  public static final String COL_CONSULT = "CONSULTATION";
+  private static final String CREATE_TAB = "CREATE TABLE " + TABLE_NAME +
+" (" + COL_ID + " INTEGER PRIMARY KEY AUTOINCREMENT, "
++ COL_NAME + " TEXT NOT NULL, "
++ COL_CONSULT + " TEXT NOT NULL);";
+
+  public SqLiteHelper(Context context) {
+    super(context, "mydatabase.db", null, 1);
+  }
+
+  @Override
+  public void onCreate(SQLiteDatabase db) {
+    db.execSQL(CREATE_TAB);
+  }
+
+  @Override
+  public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+    db.execSQL("DROP TABLE IF EXISTS " + TABLE_NAME);
+    onCreate(db);
+  }
+} 
