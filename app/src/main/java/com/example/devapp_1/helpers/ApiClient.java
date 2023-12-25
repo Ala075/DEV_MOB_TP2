ebbe724c183f35ade4b7907a197afa2d84ced003
Ala Arfaoui <128066189+Ala075@users.noreply.github.com>
2023-12-25 16:02:44 +0100

    update ApiClient.java

--- app/src/main/java/com/example/devapp_1/helpers/ApiClient.java
+++ app/src/main/java/com/example/devapp_1/helpers/ApiClient.java
@@ -2,7 +2,7 @@ import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
 
 public class ApiClient {
-    private static final String BASE_URL = "http://10.0.2.2:3000/";
+    private static final String BASE_URL = "https://api-vs0.vercel.app/";
     private static Retrofit retrofit;
 
     public static Retrofit getApiClient() {
