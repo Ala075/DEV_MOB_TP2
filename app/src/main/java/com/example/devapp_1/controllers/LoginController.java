2d06d1ecab00cdb325d2e238d0e3d15c9b17f91b
Ala Arfaoui <128066189+Ala075@users.noreply.github.com>
2023-12-25 17:23:42 +0100

    update LoginController.java

--- app/src/main/java/com/example/devapp_1/controllers/LoginController.java
+++ app/src/main/java/com/example/devapp_1/controllers/LoginController.java
@@ -1,8 +1,6 @@
 package com.example.devapp_1.controllers;
 
 import android.content.Context;
-import android.content.SharedPreferences;
-
 import com.example.devapp_1.models.User;
 
 public class LoginController {
@@ -11,7 +9,6 @@ public class LoginController {
     private static User user;
 
     private static LoginController instance;
-    private static final String shared_Pref = "sharedPref";
 
     private LoginController() {
         super();
@@ -24,25 +21,28 @@ public class LoginController {
         return instance;
     }
 
-    private static void recapUser (Context context) {
-        SharedPreferences sharedPreferences = context.getSharedPreferences(shared_Pref, Context.MODE_PRIVATE);
-        String Username = sharedPreferences.getString("Username","");
-        String Password = sharedPreferences.getString("password","");
-        user = new User(Username, Password);
+    private static void recapUser (Context context) {Retrofit retrofit = ApiClient.getApiClient();
+      ApiService apiService = retrofit.create(ApiService.class);
+
+      Call<String> call = apiService.getHelloWorld();
+      call.enqueue(new Callback<User>() {
+            @Override
+            public void onResponse(Call<User> call, Response<User> response) {
+                if (response.isSuccessful()) {
+                    user = response.body();
+                }
+            }
+
+            @Override
+            public void onFailure(Call<User> call, Throwable t) {
+                user = new User("Error", "Error");
+            }
+        });
+      
     }
 
-    public void CreateUser(String Username, String password, Context context) {
-        //création de l'utilisateur
-        user = new User(Username, password);
-        //persistance de données de l'utilisateur
-        SharedPreferences sharedPreferences = context.getSharedPreferences(shared_Pref, Context.MODE_PRIVATE);
-        SharedPreferences.Editor editor = sharedPreferences.edit();
-        editor.putString("Username" , Username);
-        editor.putString("password" ,password);
-        editor.apply();
-    }
-
-    public String getUsername() { return user.getUsername();  }
+    public String getEmail() { return user.getEmail();  }
 
     public String getPassword() { return user.getPassword(); }
+
 }
